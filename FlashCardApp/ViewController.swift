40d7e34f65f8bb64859c1efa0e4efdda1200d8b5
Yumibyte <ashraigosa@gmail.com>
2019-09-08 19:10:24 -0700

    Initial Commit

---
+++ FlashCardApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashCardApp
+//
+//  Created by Ashley Raigosa on 9/8/19.
+//  Copyright © 2019 Ashley Raigosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
